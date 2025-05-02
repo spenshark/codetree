078287f77f06f2506aaa030f079b19c71e80a866
spenshark <65120719+spenshark@users.noreply.github.com>
2025-05-02 10:19:37 +0900

    Title: 점수 비교, Time: 142ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250502/점수 비교/score-comparison.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A_math_score = sc.nextInt();
+        int B_math_score = sc.nextInt();
+        int A_english_score = sc.nextInt();
+        int B_english_score = sc.nextInt();
+
+        if(A_math_score > B_math_score && A_english_score > B_english_score){
+            System.out.print("1");
+        } else{
+            System.out.print("0");
+        }
+    }
+}

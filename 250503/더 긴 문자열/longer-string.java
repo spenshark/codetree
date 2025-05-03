8d5b7f07b752773baaf3195aa4697517271a25c8
spenshark <65120719+spenshark@users.noreply.github.com>
2025-05-03 21:17:43 +0900

    Title: 더 긴 문자열, Time: 119ms, Memory: 10MB, Status: Passed - Codetree

--- 250503/더 긴 문자열/longer-string.java
+++ 250503/더 긴 문자열/longer-string.java
@@ -2,9 +2,21 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        String word1 = sc.next();
+        String word2 = sc.next();
         
-        
+        if(word1.length() == word2.length()){
+            System.out.println("same");
+        } else {
+            if(word1.length() > word2.length()){
+            System.out.println(word1 + " " + word1.length());
+            } else {
+            System.out.println(word2 + " " + word2.length());
+            }
+        }
+
         // Please write your code here.
     }
 }

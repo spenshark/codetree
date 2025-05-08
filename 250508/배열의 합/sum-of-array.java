e0f34b8a31477c5b89359150b4a88ef77cdca025
spenshark <65120719+spenshark@users.noreply.github.com>
2025-05-08 18:22:58 +0900

    Title: 배열의 합, Time: 86ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250508/배열의 합/sum-of-array.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int total = 0;
+
+        for (int i = 0; i < 4; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 4; j++) {
+                total += Integer.parseInt(st.nextToken());
+            }
+            sb.append(total).append("\n");
+            total = 0;
+        }
+
+        System.out.println(sb);
+    }
+}

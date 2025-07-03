e49dec7912bbbb339b7274a61247c58d557b05f0
spenshark <65120719+spenshark@users.noreply.github.com>
2025-07-03 23:33:56 +0900

    Title: 함수를 이용한 369 게임, Time: 66ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250703/함수를 이용한 369 게임/369-games-using-functions.py
@@ -0,0 +1,12 @@
+a, b = map(int, input().split())
+cnt = 0
+
+def is_div_3(num):
+    if(num // 10 % 3 == 0 or num%3 == 0 or num % 10 % 3 == 0):
+        return True
+
+for i in range(a, b+1):
+    if(is_div_3(i)):
+        cnt += 1
+
+print(cnt)

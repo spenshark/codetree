e56060229cc8330b0f9cb5a72c48843292c006d8
spenshark <65120719+spenshark@users.noreply.github.com>
2025-07-04 09:56:38 +0900

    Title: 사칙연산 함수, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250704/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,14 @@
+a, o, c = input().split()
+a = int(a)
+c = int(c)
+
+if o == '+':
+    print(f'{a} {o} {c} = {a+c}')
+elif o == '-':
+    print(f'{a} {o} {c} = {a-c}')
+elif o == '*':
+    print(f'{a} {o} {c} = {a*c}')
+elif o == '/':
+    print(f'{a} {o} {c} = {a/c}')
+else:
+    print("False")

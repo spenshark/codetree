138c88c4e87e46b7f1988403bf38d5a042fdc7ae
spenshark <65120719+spenshark@users.noreply.github.com>
2025-07-08 15:00:20 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 81ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250708/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -0,0 +1,28 @@
+n = int(input())
+arr = list(map(int, input().split()))
+lcm = 0
+
+def gcd(a,b):
+    if a % b == 0:
+        return b
+    
+    if a > b:
+        return gcd(b, a%b)
+    else:
+        return gcd(a, b%a)
+
+def temp(n):
+    global lcm
+    if n < 1:
+        return
+    
+    num1 = arr[n-1]
+    num2 = arr[n-2]
+
+    lcm = (num1 * num2) // gcd(num1, num2)
+    arr[n-2] = lcm
+
+    return temp(n-1)
+
+temp(n)
+print(lcm)
